ebbe00b4dabe15098394e0065663a77eb42b6d94
Dima_Kaleinik <dim040489@gmail.com>
2021-04-05 16:48:08 +0300

    first commit

---
+++ src/skills/Skills.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import style from './Skills.module.css';
+import styleContainer from "../../src/common/styles/Container.module.css"
+import Skill from "./skill/Skill";
+
+const Skills = () => {
+    return (
+        <div className={style.skillsBlock}>
+            <div className={`${styleContainer.container} ${style.skillsContainer}`}>
+                <h2 className={style.title}> Skills </h2>
+                <div className={style.skills}>
+                    <Skill title={"HTML"} description={"Create encapsulated components with your own state and then merge them into complex user interfaces."}/>
+                    <Skill title={"JS"} description={"Create encapsulated components with your own state and then merge them into complex user interfaces."}/>
+                    <Skill title={"REACT"} description={"Create encapsulated components with your own state and then merge them into complex user interfaces."}/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Skills;
